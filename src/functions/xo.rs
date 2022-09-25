3e436acd8febbe3fd143a423f939eaf1fd7527ab
Bonito Aubrey <bonitoaubrey@gmail.com>
2022-09-25 18:08:15 +0300

    check xo function with rustfmt

--- src/functions/xo.rs
+++ src/functions/xo.rs
@@ -3,8 +3,12 @@ pub fn xo(string: &'static str) -> bool {
     let mut o = 0;
 
     for i in string.to_lowercase().as_bytes() {
-        if *i == b'x' {x += 1}
-        if *i == b'o' {o += 1}
+        if *i == b'x' {
+            x += 1
+        }
+        if *i == b'o' {
+            o += 1
+        }
     }
     x == o
 }
